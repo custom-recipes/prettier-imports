216bc5d3c968283806cb98f77c2ecac5077b3d9e
Stefan Kühnel <git@stefankuehnel.com>
2022-03-07 18:55:02 +0100

    feat: initial commit

---
+++ index.ts
@@ -0,0 +1,68 @@
+import Filehound from "filehound"
+import { RecipeBuilder, paths } from "@blitzjs/installer"
+import { sep as pathSeparator } from "path"
+
+const pluginConfig = {
+    importOrder: () => {
+        // Directories to discard.
+        const discardNodeModules = "node_modules"
+        const discardPrismaDatabaseMigrations = "db.*migrations.*\\d.*$"
+
+        // Derive importOrder configuration from project directory structure.
+        const directoryStructure = Filehound.create()
+            .path(".")
+            .discard([discardNodeModules, discardPrismaDatabaseMigrations])
+            .ignoreHiddenDirectories()
+            .ignoreHiddenFiles()
+            .directory()
+            .findSync()
+
+        const optionConfig = directoryStructure.map((path) => {
+            // Replace \\ or \ with / in path.
+            const configPath = path.split(pathSeparator).join("/")
+
+            // See: https://github.com/trivago/prettier-plugin-sort-imports#importorder
+            return `^${configPath}/(.*)$`
+        })
+
+        return optionConfig
+    },
+    importOrderSeparation: true,
+    importOrderSortSpecifiers: true,
+    importOrderGroupNamespaceSpecifiers: true,
+}
+
+export default RecipeBuilder()
+    .setName("Prettier Imports")
+    .setDescription("This will install all necessary dependencies and configure the Prettier plugin '@trivago/prettier-plugin-sort-imports' for use.")
+    .setOwner("Stefan Kühnel <git@stefankuehnel.com>")
+    .setRepoLink("https://github.com/custom-recipes/prettier-imports")
+    .addAddDependenciesStep({
+        stepId: "addDeps",
+        stepName: "Add npm dependencies",
+        explanation: "We'll install the Prettier plugin itself.",
+        packages: [
+            {
+                name: "@trivago/prettier-plugin-sort-imports",
+                version: "latest",
+                isDevDep: true
+            }
+        ]
+    })
+    .addTransformFilesStep({
+        stepId: "configurePrettierPlugin",
+        stepName: "Configure Prettier plugin",
+        explanation: "Next, we'll configure the prettier plugin based on your directory structure.",
+        singleFileSearch: paths.packageJson(),
+        transformPlain(plainTextPackageJson: string) {
+            const packageJson = JSON.parse(plainTextPackageJson);
+
+            packageJson.prettier.importOrder = pluginConfig.importOrder();
+            packageJson.prettier.importOrderSeparation = pluginConfig.importOrderSeparation;
+            packageJson.prettier.importOrderSortSpecifiers = pluginConfig.importOrderSortSpecifiers;
+            packageJson.prettier.importOrderGroupNamespaceSpecifiers = pluginConfig.importOrderGroupNamespaceSpecifiers;
+
+            return JSON.stringify(packageJson);
+        }
+    })
+    .build()
---
+++ readme.md
@@ -0,0 +1,23 @@
+# prettier-imports
+
+[![Commitizen friendly](https://img.shields.io/badge/commitizen-friendly-brightgreen.svg)](http://commitizen.github.io/cz-cli/)
+
+The Blitz.js Recipe for installing [`@trivago/prettier-plugin-sort-imports`](https://github.com/trivago/prettier-plugin-sort-imports).
+
+```sh
+blitz install custom-recipes/prettier-imports -y
+```
+
+## More information
+
+- [How to use receipes in Blitz](https://blitzjs.com/docs/using-recipes)
+- [Learn about `@trivago/prettier-plugin-sort-imports`](https://tech.trivago.com/post/2021-12-17-aprettierpluginthatsortsyourimports/)
+- [`@trivago/prettier-plugin-sort-imports`'s Github](https://github.com/trivago/prettier-plugin-sort-imports)
+
+## Found a bug?
+
+Thanks for letting us know! Please [file an issue](../../issues/new?assignees=&labels=&template=bug_report.md&title=) and we should reply shortly.
+
+## License
+
+MIT License, see the included [license.md](license.md) file.
